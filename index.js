9db74006a5b1c01c0791d7211fee9102f756dbf6
Ceasonal <69846257+Ceasonal@users.noreply.github.com>
2023-03-17 13:41:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits, ActivityType } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds,
+	GatewayIntentBits.GuildMessages,
+	GatewayIntentBits.MessageContent,
+	GatewayIntentBits.GuildMembers,] });
+
+// Commands file
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+//read all folders and commands in command folder
+
+
+client.on('ready', () => {
+	client.user.setPresence({
+	  activities: [{ name: 'Bugs', type: ActivityType.Watching }],
+	  status: 'online',
+	});
+  });
+
+// Log in to Discord with your client's token
+client.login(token);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
